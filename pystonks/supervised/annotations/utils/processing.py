13ec439894146b44ea154a8201450b79848cd80f
aaron-jencks <aaronjencks@aol.com>
2024-09-02 20:54:37 -0400

    fixed scaling issue

--- pystonks/supervised/annotations/utils/processing.py
+++ pystonks/supervised/annotations/utils/processing.py
@@ -11,13 +11,7 @@ def place_on_avg(
     if state.is_zoomed:
         cmn, cmx = state.zoom_lim
         closes = [c for t, c in zip(data.times, data.closes) if cmn <= t <= cmx]
-        zvalues = [v for t, v in zip(times, values) if cmn <= t <= cmx]
-        vmin = min(zvalues)
-        vmax = max(zvalues)
-    else:
-        vmin = min(values)
-        vmax = max(values)
 
     cavg = sum(closes) / len(closes)
-    vdiff = (vmax - vmin) / 2
+    vdiff = (max(values) - min(values)) / 2
     return [v - vdiff + cavg for v in values]
