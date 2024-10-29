dbd42a0ffb2ee53d5116f8ccde3ae4ea98032de1
aaron-jencks <aaronjencks@aol.com>
2024-10-29 10:55:55 -0400

    documented gui definitions

--- pystonks/utils/gui/definitions.py
+++ pystonks/utils/gui/definitions.py
@@ -1,5 +1,16 @@
 from typing import Callable, Any
 
 DARK_MODE_COLOR = '#565956'
+"""
+The dark mode color definition for GUI programs
+"""
+
 BUTTON_HANDLER = Callable[[], None]
+"""
+A function that can handle button presses
+"""
+
 EVENT_HANDLER = Callable[[Any], None]
+"""
+A function that can handle generic events
+"""
